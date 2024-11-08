7f35f275680ebd081ce901ddaa45009f47de3761
Krystal304 <krystalwilliams864@gmail.com>
2024-11-08 13:57:41 -0500

    initial

---
+++ routes/produceRoutes.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+import produceCTRL from '../controllers/produceControllers.mjs';
+
+const router = express.Router();
+
+// Create/ReadALl
+router
+  .route('/')
+  .post(produceCTRL.createProduce)
+  .get(produceCTRL.getAllProduce);
+
+// Update/Delete
+router
+  .route('/:id')
+  .put(produceCTRL.updateOneProduce)
+  .delete(produceCTRL.deleteOneProduce)
+  .get(produceCTRL.getOneProduce);
+
+// router.route('/seed').get(produceCTRL.seedDB);
+
+export default router;
